0d100914744c6cfa39dc097493dcf3b751191a59
unknown <1784929126@qq.com>
2023-08-21 23:48:17 +0800

    Update config class and use this package to simplize the config

--- main/train.py
+++ main/train.py
@@ -4,24 +4,24 @@ from torch import nn, optim
 from model.IO_NewUser import BinaryClassifier
 from tools.load_data import load_data
 
+from config import Config
+
+config = Config()
+
 # 将数据转换为合适的形状，即 (batch_size, input_size)
-data_loader = load_data("../data_generate/train_unknown.pt")
+data_loader = load_data(config.train_unknown_pt)
 
 # 使用模型进行预测
-
 # 输入特征的维度
-input_size = 10
-
 # 创建模型实例
-model = BinaryClassifier(input_size)
+model = BinaryClassifier(config.input_size)
 
 # 定义损失函数和优化器
 criterion = nn.BCELoss()  # 二元交叉熵损失函数
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=config.lr)
 
-num_epochs = 100
-for epoch in range(num_epochs):
-    print(f"Epoch {epoch + 1}/{num_epochs}")
+for epoch in range(config.num_epochs):
+    print(f"Epoch {epoch + 1}/{config.num_epochs}")
     for batch in data_loader:
         # 提取特征和标签
         features = batch['features']
